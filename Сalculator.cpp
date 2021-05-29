8e7fe71debbf50d1fbcdc12538ed85eb16c5dd30
Artemiy <los28.2001@mail.ru>
2021-05-29 16:52:38 +0500

    Input of numbers and operation character

---
+++ Сalculator.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int firstNumber, secondNumber;
+    char operationCharacter;
+    cout << "Enter first number\n";
+    cin >> firstNumber;
+    cout << "enter operation character\n";
+    cin >> operationCharacter;
+    cout << "Enter second number\n";
+    cin >> secondNumber;
+}
